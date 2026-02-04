0c1f58ead089b2517445bb9d7b86e1064b49d738
MKvaratshelia <kvaratsheliamihail@yandex.ru>
2026-02-04 10:08:34 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="main"></div>
+</template>
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.main {
+    display: flex;
+    min-width: 1200px;
+    flex-direction: column;
+    padding: 20px;
+}
+</style>
---
+++ src/icons/IconPlus.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+<template>
+    <svg
+        width="14"
+        height="14"
+        viewBox="0 0 14 14"
+        fill="black"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            d="M7.00001 1.88V12.12M12.12 7.00001L1.88 7.00001"
+            stroke="black"
+            stroke-width="2"
+            stroke-linecap="round"
+        />
+    </svg>
+</template>
+
+<style lang="scss" scoped></style>
